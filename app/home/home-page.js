1b9999529abb00dc2419f1d15c5f452caa2e8eff
Martin Bektchiev <martin.bektchiev@progress.com>
2018-08-21 19:06:53 +0300

    Run Octane benchmarks
    * Run all benchmarks except typescript (it needs more effort in rewriting dependencies to `fs`, `path`, `module`
    * Present results in labels
    * `console.log` results for reading from scripts

--- app/home/home-page.js
+++ app/home/home-page.js
@@ -9,6 +9,90 @@ const HomeViewModel = require("./home-view-model");
 function onNavigatingTo(args) {
     const page = args.object;
     page.bindingContext = new HomeViewModel();
+
+    launchTests(page.bindingContext);
 }
+
+function launchTests(viewModel) {
+    // <!-- Octane benchmark code -->
+    require("./octane/base.js");
+    require("./octane/richards.js");
+    require("./octane/deltablue.js");
+    require("./octane/crypto.js");
+    require("./octane/raytrace.js");
+    require("./octane/earley-boyer.js");
+    require("./octane/regexp.js");
+    require("./octane/splay.js");
+    require("./octane/navier-stokes.js");
+    require("./octane/pdfjs.js");
+    require("./octane/mandreel.js");
+    require("./octane/gbemu-part1.js");
+    require("./octane/gbemu-part2.js");
+    require("./octane/code-load.js");
+    require("./octane/box2d.js");
+    require("./octane/zlib.js");
+    require("./octane/zlib-data.js");
+    // require("./octane/typescript.js");
+    // require("./octane/typescript-input.js");
+    // require("./octane/typescript-compiler.js");
+
+    var completed = 0;
+    var benchmarks = BenchmarkSuite.CountBenchmarks();
+    var success = true;
+    var latencyBenchmarks = ["Splay", "Mandreel"];
+    var skipBenchmarks =
+            typeof skipBenchmarks === "undefined" ? [] : skipBenchmarks;
+
+    function log(msg) {
+        console.log(msg);
+        viewModel.set("messages", viewModel.messages + msg);
+    }
+
+    function ShowBox(name) {
+      log(`Starting ${name}\n`);
+    }
+    function AddResult(name, result) {
+      log(name + ': ' + result + "\n");
+    }
+
+    function AddError(name, error) {
+      log(name + ": " + error.message + "\n");
+      success = false;
+    }
+
+    function AddScore(score) {
+      let scoreMsg = success
+        ? `Octane Score: ${score}`
+        : `Octane Score (incomplete): ${score}`;
+      viewModel.set("score", scoreMsg);
+      log(scoreMsg);
+    }
+
+    function Run() {
+      BenchmarkSuite.RunSuites({
+        NotifyStart : ShowBox,
+        NotifyError : AddError,
+        NotifyResult : AddResult,
+        NotifyScore : AddScore
+      },
+      skipBenchmarks);
+    }
+
+    function CheckCompatibility() {
+      // If no Typed Arrays support, show warning label.
+      var hasTypedArrays = typeof Uint8Array != "undefined"
+          && typeof Float64Array != "undefined"
+          && typeof (new Uint8Array(0)).subarray != "undefined";
+
+      if (!hasTypedArrays) {
+        console.log("Typed Arrays not supported");
+      }
+
+      Run();
+    }
+
+    const timer = require('timer');
+    timer.setTimeout(CheckCompatibility, 200);
+  }
 
 exports.onNavigatingTo = onNavigatingTo;
--- app/home/home-view-model.js
+++ app/home/home-view-model.js
@@ -3,6 +3,8 @@ const observableModule = require("data/observable");
 function HomeViewModel() {
     const viewModel = observableModule.fromObject({
         /* Add your view model properties here */
+        messages: "Individual test results:\n",
+        score: 'Octane tests are running...'
     });
 
     return viewModel;
